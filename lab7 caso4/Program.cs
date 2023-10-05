16beb08ff11f60ab511cf1d5541e77fba995f925
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-10-04 23:45:22 -0500

    Agregar archivos de proyecto.

---
+++ lab7 caso4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7_caso4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero de lineas: ");
+            int lineas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lineas; i++) 
+            {
+                Console.Write("Ingresa un numero: ");
+                int num = int.Parse(Console.ReadLine());
+
+                for (int j =0; j < num; j++) 
+                {
+                    for (int k = 0; k < num*2; k++) 
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
